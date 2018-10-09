b5d9e84f1372b6a741156103351dc0e35c11aae4
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-10-09 11:07:44 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart= []
 
   def call(env)
     resp = Rack::Response.new
@@ -10,9 +11,37 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      if @@items.include?(item)
+        @@cart << item 
+        resp.write "Successfully added #{item}"
+      else
+        resp.write "We don't have that item"
+      end
+
     elsif req.path.match(/search/)
+
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+      ## WHAT KIND OF METHOD IS 'HANDLE_SEARCH'?
+      ##                          = > class instance method
+      ## but if it is a class instance method..
+      ## only an instance of the class may use it
+      ## here, what is using it?
+      ## the method is called by ...?
+
     else
       resp.write "Path Not Found"
     end
@@ -27,4 +56,5 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
 end
